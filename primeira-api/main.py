b6b9755b149234cb7962ae1da2324719143666de
Ismael Cleiton <ismaelcleitonsf@gmail.com>
2023-04-27 20:17:27 -0300

    atualização

--- primeira-api/main.py
+++ primeira-api/main.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
+from markupsafe import escape
 
-app = Flask('tesseract')
+app = Flask('Training')
 
+@app.route('/')
+def helloworld():
+    return "<p>Hello World</p>"
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name = None):
+    return render_template('hello.html',name = name)
+
+@app.route('/projects/')
+def projects():
+    return  projects
 app.run()
--- projeto1/main.py
+++ projeto1/main.py
@@ -1,1 +1,7 @@
+import math
+
 print('Projeto 1')
+
+print((4/3)*math.pi*(5**3))
+print(5**2)
+print(2**2)
